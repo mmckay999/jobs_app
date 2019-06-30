4931ce68dce39cb0fe331410ed45b4bdfee5e0bc
mmckay999 <36682659+mmckay999@users.noreply.github.com>
2019-06-30 18:53:53 -0400

    tetsing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require turbolinks
 //= require jquery
 //= require jquery_ujs
+//= require turbolinks
 //= requiire bootstrap
 //= require bootstrap-sprockets
 //= require_tree .
